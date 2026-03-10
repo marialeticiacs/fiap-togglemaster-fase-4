cfa1d561274ed464c7f1dc8ca4ee4c99ea0a8c8e
Maria Letícia <marialeticia0306@gmail.com>
2026-03-09 22:25:19 -0300

    add argocd.tf

---
+++ terraform/argocd.tf
@@ -0,0 +1,13 @@
+resource "helm_release" "argocd" {
+  name             = "argocd"
+  repository       = "https://argoproj.github.io/argo-helm"
+  chart            = "argo-cd"
+  namespace        = "argocd"
+  create_namespace = true
+  version          = "5.51.6" 
+
+  set {
+    name  = "server.service.type"
+    value = "ClusterIP"
+  }
+}
